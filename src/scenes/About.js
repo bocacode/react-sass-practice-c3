3df57fc40a2590433dee830723f0769cd9f41461
jonathans199 <jonathans199@gmail.com>
2021-08-03 17:28:20 -0400

    updated video

--- src/scenes/About.js
+++ src/scenes/About.js
@@ -1,11 +1,11 @@
-// import videoUrl from './../assets/video.mp4'
+import videoUrl from './../assets/video.mp4'
 
 export default function About() {
   return (
     <div className='bg__video'>
-      {/* <video src={videoUrl} className='bg__video--content' autoPlay loop muted>
+      <video src={videoUrl} className='bg__video--content' autoPlay loop muted>
         <source src={videoUrl} type='video/mp4' />
-      </video> */}
+      </video>
     </div>
   )
 }
